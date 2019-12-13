305ee0399ee7f49295c86a752587b14e0fd129d1
jesus1920970 <jesus192219970@gmail.com>
2019-12-13 11:40:14 -0600

    fixed-2

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ const Env = use('Env')
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
+
 const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 module.exports = {
   /*
@@ -70,14 +71,14 @@ module.exports = {
   | npm i --save pg
   |
   */
-  pg: {
-    client: 'pg',
-    connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.hostname),
-      port: Env.get('DB_PORT', DATABASE_URL.port),
-      user: Env.get('DB_USER', DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
-    },
-  }
+ pg: {
+  client: 'pg',
+  connection: {
+    host: Env.get('DB_HOST', DATABASE_URL.hostname),
+    port: Env.get('DB_PORT', DATABASE_URL.port),
+    user: Env.get('DB_USER', DATABASE_URL.username),
+    password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+    database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+  },
+}
 }
